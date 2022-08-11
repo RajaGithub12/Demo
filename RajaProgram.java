d794df8cea018f00e2f30c7083ee20cd5aa4ebeb
Raja <110146527+RajaGithub12@users.noreply.github.com>
2022-08-11 12:15:11 +0530

    Delete RajaProgram.java

--- RajaProgram.java
+++
@@ -1,9 +0,0 @@
-#new changes
-public class studyopedia {
- 
-       public static void main(String []args) {
-             
-                   System.out.println("Java 18 First Program.");
-
-   }
-}
